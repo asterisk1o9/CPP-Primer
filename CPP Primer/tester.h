847eaa8fb98de203460653a24c2cbdb7678577eb
maugu028 <maugu028@fiu.edu>
2022-05-10 01:26:04 -0400

    Add project files.

---
+++ CPP Primer/tester.h
@@ -0,0 +1,24 @@
+#pragma once
+//
+//  tester.hpp
+//  CppPrimer
+//
+//  Created by Marvin Augustin on 11/9/21.
+//
+
+#ifndef tester_h
+#define tester_h
+
+#include <stdio.h>
+
+#endif /* tester_h */
+
+//extern const int bufSize = fcn();
+extern const int bufSize;
+
+class tester {
+public:
+    tester(int v);
+    int val;
+    int absolute();
+};
